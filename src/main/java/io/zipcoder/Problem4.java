3929ffc615352025022c2022a7c7e2a689f5ae0b
Keith Brinker <keithbrinker@zipcoders-MBP-2.fios-router.home>
2018-04-15 16:47:47 -0400

    Tests written, method stubbed out, gametime

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,10 @@
 package io.zipcoder;
 
 public class Problem4 {
+
+    public static String anagramCheck(String input){
+
+        return null;
+    }
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,44 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    @Test
+    public void potentialAnagramTest() {
+        //given
+        String input = "aaabbbb";
+
+        //when
+        String actual = Problem4.anagramCheck(input);
+        //then
+        String expected = "YES";
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void potentialAnagramTestTwo() {
+        //given
+        String input = "cdefghmnopqrstuvw";
+
+        //when
+        String actual = Problem4.anagramCheck(input);
+        //then
+        String expected = "NO";
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void potentialAnagramTestThree() {
+        //given
+        String input = "cdcdcdcdeeeef";
+        //when
+        String actual = Problem4.anagramCheck(input);
+        //then
+        String expected = "YES";
+        Assert.assertEquals(actual, expected);
+    }
+
 }
+
